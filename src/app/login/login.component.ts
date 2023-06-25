24bd7088f10b541ff6bc368e5115fe1c3679d90a
Gazi Monir-Uz-Zaman Apu <ciphercall@gmail.com>
2023-06-25 12:32:41 +0600

    login function
    matching from json file implemented

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,7 +5,12 @@ import {FormsModule} from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+import credentials from '../../assets/credentials.json';
 
+interface Credential {
+  usr: string;
+  pass: string;
+}
 
 @Component({
   selector: 'app-login',
@@ -15,11 +20,21 @@ import {MatButtonModule} from '@angular/material/button';
   imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatDividerModule, MatIconModule]
 })
 export class LoginComponent {
+  credentials: Credential[] = credentials;
   username!: string;
   password!: string;
 
   login() {
-    console.log(`Username: ${this.username}`);
-    console.log(`Password: ${this.password}`);
+    const user = this.credentials.find(
+      credential =>
+        credential.usr === this.username && credential.pass === this.password
+    );
+    if (user) {
+      // Login successful
+      console.log("Login successful!");
+    } else {
+      // Login failed
+      console.log("Login failed!");
+    }
   }
 }
