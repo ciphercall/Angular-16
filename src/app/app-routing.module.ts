d91659ee087163e00659c8c32bf118e793ee6cba
Gazi Monir-Uz-Zaman Apu <ciphercall@gmail.com>
2023-06-25 11:21:25 +0600

    Login function
    initialized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { TableComponent } from './table/table.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'home', pathMatch:'full'},
   {path:'home', component: HomeComponent, title:'Home'},
   {path:'about', component: AboutComponent, title: "About"},
-  {path:'table', component: TableComponent, title: "Table"}
+  {path:'table', component: TableComponent, title: "Table"},
+  {path:'login', component: LoginComponent, title: "Login"}
 ];
 
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @Component({
@@ -9,8 +12,14 @@ import {FormsModule} from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule]
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatDividerModule, MatIconModule]
 })
 export class LoginComponent {
+  username!: string;
+  password!: string;
 
+  login() {
+    console.log(`Username: ${this.username}`);
+    console.log(`Password: ${this.password}`);
+  }
 }
