684cc620202eac52c12b51a5e98f13c718b475d6
Gazi Monir-Uz-Zaman Apu <ciphercall@gmail.com>
2023-07-08 11:57:42 +0600

    Database Config
    columns are loading as expected

--- src/app/dbconnector/dbconnector.component.ts
+++ src/app/dbconnector/dbconnector.component.ts
@@ -9,6 +9,7 @@ import { HttpClient } from '@angular/common/http';
 export class DBConnectorComponent {
   databaseNames: string[] = [];
   tableNames: string[] = [];
+  columnNames: string[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -27,12 +28,22 @@ export class DBConnectorComponent {
     }
   }
 
-
   async displayTableNames(serverLocation: string, databaseName: string) {
     try {
       const result = await this.http.get<string[]>(`http://localhost:3000/table-names?serverLocation=${serverLocation}&databaseName=${databaseName}`).toPromise();
       if (result) {
         this.tableNames = result;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async displayColumnNames(serverLocation: string, databaseName: string, tableName: string) {
+    try {
+      const result = await this.http.get<string[]>(`http://localhost:3000/column-names?serverLocation=${serverLocation}&databaseName=${databaseName}&tableName=${tableName}`).toPromise();
+      if (result) {
+        this.columnNames = result;
       }
     } catch (err) {
       console.error(err);
--- src/assets/sql-node.js
+++ src/assets/sql-node.js
@@ -53,7 +53,7 @@ app.get('/table-names', async (req, res) => {
     const config = {
       user: "sa",
       password: "sa@123",
-      database: databaseName,
+      database: "master",
       server: serverLocation,
       pool: {
         max: 10,
@@ -70,13 +70,52 @@ app.get('/table-names', async (req, res) => {
     await sql.connect(config);
 
     // Query the table names
-    const result = await sql.query`SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'`;
+    const result = await sql.query(`SELECT TABLE_NAME FROM [${databaseName}].INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'`);
 
     // Return the table names
     res.json(result.recordset.map((row) => row.TABLE_NAME));
   } catch (err) {
     console.error(err);
     res.status(500).send('An error occurred while retrieving the table names.');
+  }
+});
+
+app.get('/column-names', async (req, res) => {
+  const serverLocation = req.query.serverLocation;
+  const databaseName = req.query.databaseName;
+  const tableName = req.query.tableName;
+
+  try {
+    // Configure the connection to the SQL server
+    const config = {
+      user: "sa",
+      password: "sa@123",
+      database: databaseName,
+      server: serverLocation,
+      pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+      },
+      options: {
+        encrypt: false,
+        trustServerCertificate: true
+      }
+    };
+
+    // Connect to the SQL server
+    await sql.connect(config);
+
+    // Query the column names
+    const result = await sql.query(`SELECT COLUMN_NAME FROM [${databaseName}].INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '${tableName}'`);
+    console.log('this is the table name: '+tableName);
+    console.log('this is the query: '+`SELECT COLUMN_NAME FROM [${databaseName}].INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '${tableName}'`);
+    console.log('this is total result: '+JSON.stringify(result));
+    // Return the column names
+    res.json(result.recordset.map((row) => row.COLUMN_NAME));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('An error occurred while retrieving the column names.');
   }
 });
 
